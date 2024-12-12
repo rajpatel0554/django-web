546454ccf21c790b1dd7dc0674be5e062c00696a
rajpatel0554 <rajpatel.0554@gmail.com>
2024-12-12 11:27:03 +0530

    V2
    v2

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("",views.homepage),
+    path("",views.demopage),
+    path("home",views.homepage),
     path("about",views.aboutpage),
     path("contact",views.contactpage),
+    path("form",views.frompage),
+    path("process",views.frompageprocess),
+    path("mark",views.subpage),
+    path("sum",views.sumpage),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def homepage(request):
+def demopage(request):
     return HttpResponse("Hello! This is my first Website.")
+
+def homepage(request):
+    return render(request,'home.html')
 
 def aboutpage(request):
     return render(request,'about.html')
@@ -10,3 +13,33 @@ def aboutpage(request):
 def contactpage(request):
     return render(request,'contact.html')
 
+def frompage(request):
+    return render(request,'form.html')
+
+def frompageprocess(request):
+    a = int(request.POST["txt1"])
+    b = int(request.POST["txt2"])
+    c = a+b
+    return render (request,"ans.html",{'mya': a , 'myb': b, 'myc' : c})
+
+def subpage(request):
+    return render(request,'marks.html')
+
+def sumpage(request):
+    s1 = int(request.POST["sub1"])
+    s2 = int(request.POST["sub2"])
+    s3 = int(request.POST["sub3"])
+    s4 = int(request.POST["sub4"])
+    s5 = int(request.POST["sub5"])
+    sum = s1+s2+s3+s4+s5
+    avg= sum/5
+    
+    if avg>80:
+        print("pass")
+    else:
+        print("Fail")
+        msg=""
+        
+    return render(request,"sum.html",{'mys1':s1 , 'mys2':s2, 'mys3':s3,'mys4':s4,'mys5':s5,'sum':sum,'avg':avg})
+    
+    
